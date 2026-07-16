18b5b52abb8b09ff4731dc88103c3984ede9bdb9
Student-Prabin <dahalprabin74@gmail.com>
2026-07-16 09:47:29 +0545

    added cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -27,7 +27,7 @@ const AboutSection = () => {
             <div className='flex flex-col sm:flex-row gap-4 pt-4 justify-center'>
               <a href="#contact" className='cosmic-button'>Get In Touch</a>
 
-              <a href="#contact" className='px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/20 transition-colors duration-300'>Download CV</a>
+              <a  href="/certificates/CV-Prabin Dahal.pdf"  download="Prabin Dahal.pdf" className='px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/20 transition-colors duration-300'>Download CV</a>
 
             </div>
 
